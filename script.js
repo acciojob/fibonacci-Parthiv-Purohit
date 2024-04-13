a9ec6d801d7d39e485e9d49b5f012ab4644ad471
Parthiv-Purohit <150549002+Parthiv-Purohit@users.noreply.github.com>
2024-04-13 23:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
 // your code here
+	if(num === 1 || num===2)
+	{
+		return num-1;
+	}
+	let num1 = 0;
+	let num2 = 1;
+	for(let i=1;i<=num-2;i++)
+		{
+			num3 = num1 + num2;
+			num1 = num2;
+			num2 = num3;
+		}
+	return num2;
+	
 }
 
 module.exports = fibonacci;
